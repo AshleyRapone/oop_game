5c3661fde02dcfd070a6b36af3078f2efe2c5aac
Ashley Rapone <ashleylynnrapone@gmail.com>
2021-05-26 20:58:02 -0400

    Added keyboard functionality

--- js/app.js
+++ js/app.js
@@ -6,13 +6,22 @@ const missed = 0;
 const phrases = ['Winter is coming', "You know nothing Jon Snow", "A Lannister always pays his debts", "Dracarys", "Hold the door"];
 const activePhrase = null;
 
+const keys = document.getElementsByClassName('key');
 const startGameButton = document.getElementById('btn__reset');
 startGameButton.addEventListener('click', () => {
   game = new Game(missed, phrases, activePhrase);
   game.startGame();
+  document.addEventListener('keyup', (event) => {
+    let keyPressed = event.key;
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i].innerHTML === keyPressed) {
+          game.handleInteraction(keys[i]);
+      }
+      }
+  })
+
 })
 
-const keys = document.getElementsByClassName('key');
 for (let i = 0; i < keys.length; i++) {
   keys[i].addEventListener('click', (event) => {
     game.handleInteraction(event.target);
